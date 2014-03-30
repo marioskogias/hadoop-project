bd175ead8cce6827d0211f69120fc0ec345f8ce2
Marios Kogias <marioskogias@grnet.gr>
2014-03-30 12:24:13 +0300

    Indent

--- code/aol-dataset/src/SearchCount.java
+++ code/aol-dataset/src/SearchCount.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 
-        
-
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -10,57 +8,61 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-        
-public class SearchCount {
-        
- public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-        
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-       String words[] = line.split("\t");
-       String date = words[2].split(" ")[0]; // this is YYYY-MM-DD HH:MM:SS and we need only date
-       word.set(date);
-       context.write(word, one);
-    }
- } 
-        
- public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
-      throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        context.write(key, new IntWritable(sum));
-    }
- }
-        
- public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-        
-    Job job = new Job(conf, "searchcount");
-    job.setJarByClass(SearchCount.class);
-   
-    job.setNumReduceTasks(2);
-    
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-        
-    job.setMapperClass(Map.class);
-    job.setReducerClass(Reduce.class);
-    
-    job.setPartitionerClass(MyCustomDatePartitioner.class);
-    
-    job.setInputFormatClass(TextInputFormat.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
-        
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        
-    job.waitForCompletion(true);
- }
-        
+public class SearchCount {
+
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+			String words[] = line.split("\t");
+			String date = words[2].split(" ")[0]; // this is YYYY-MM-DD HH:MM:SS
+													// and we need only date
+			word.set(date);
+			context.write(word, one);
+		}
+	}
+
+	public static class Reduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "searchcount");
+		job.setJarByClass(SearchCount.class);
+
+		job.setNumReduceTasks(2);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setPartitionerClass(MyCustomDatePartitioner.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+
 }
