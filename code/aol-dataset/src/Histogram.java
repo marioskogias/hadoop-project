21286d4a4553c539d20e7a910589e752ca53a85c
Marios Kogias <marioskogias@grnet.gr>
2014-03-31 17:23:15 +0300

    2.4: Implement distributed cache

--- code/aol-dataset/src/Histogram.java
+++ code/aol-dataset/src/Histogram.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.io.*;
 
+import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -15,10 +20,27 @@ public class Histogram {
 	public static class Map extends
 			Mapper<LongWritable, Text, IntWritable, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
-		
+		FileInputStream fileStream;
+		HashSet<String> stopWords = new HashSet<String>();
+
+		public void setup(Context context) throws IOException,
+				InterruptedException {
+
+			Configuration conf = context.getConfiguration();
+			Path[] localFiles = DistributedCache.getLocalCacheFiles(conf);
+			fileStream = new FileInputStream(localFiles[0].toString());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					fileStream));
+			String line = reader.readLine();
+			while (line != null) {
+				stopWords.add(line);
+				line = reader.readLine();
+			}
+
+		}
+
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
-			
 		}
 	}
 
@@ -27,11 +49,10 @@ public class Histogram {
 
 		public void reduce(IntWritable key, Iterable<IntWritable> values,
 				Context context) throws IOException, InterruptedException {
-		
 		}
 	}
 
-	/*we assume a single reducer*/
+	/* we assume a single reducer */
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 
@@ -40,7 +61,7 @@ public class Histogram {
 
 		job.setMapOutputKeyClass(IntWritable.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
@@ -52,6 +73,11 @@ public class Histogram {
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		/* add destributed cache */
+		DistributedCache.addCacheFile(
+				new Path("/user/root/misc/english.stop").toUri(),
+				job.getConfiguration());
 
 		job.waitForCompletion(true);
 	}
