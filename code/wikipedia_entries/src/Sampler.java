3f88dcba29807b5d44d0e087888c5d66577b5be7
Marios Kogias <marioskogias@grnet.gr>
2014-04-01 01:29:04 +0300

    Add Sampler class
    It just extends LexiSort.Mapper

---
+++ code/wikipedia_entries/src/Sampler.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+public class Sampler {
+
+	public static class Map extends LexiSort.Map {
+		@Override
+		public void run(Context context) throws IOException,
+				InterruptedException {
+			setup(context);
+
+			/*
+			 * Sample every step input lines
+			 * 
+			 * Collect target samples per mapper
+			 */
+			int step = 50;
+			int count1 = 0; // count1 step
+			int count2 = 0; // count2 target
+			int target = 50; // this is the amount we want
+			while (context.nextKeyValue()) {
+				if (count1 == step) {
+					map(context.getCurrentKey(), context.getCurrentValue(),
+							context);
+					step = -1;
+					count2++;
+					if (count2 == target)
+						break;
+				}
+			}
+		}
+	}
+
+}
