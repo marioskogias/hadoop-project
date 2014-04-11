edc87af8b7c72759a2349559f1a446e2dcbcf46a
Marios Kogias <marioskogias@grnet.gr>
2014-04-11 21:22:10 +0300

    Screwed up - Start over

--- code/Hbase-project/src/BulkLoad.java
+++ code/Hbase-project/src/BulkLoad.java
@@ -1,54 +1,44 @@
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class BulkLoad {
 
 	public static class Map extends
-			Mapper<LongWritable, Text, Text, NullWritable> {
+			Mapper<LongWritable, Text, ImmutableBytesWritable, KeyValue> {
 
-		private Text title = new Text();
-		NullWritable none = NullWritable.get();
-
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
-			String line = value.toString();
-
-			title.set(line);
-			context.write(title, none);
-		}
-	}
-
-	public static class Reduce extends Reducer<Text, NullWritable, Text, Text> {
-
-		public void reduce(Text key, Iterable<NullWritable> values,
-				Context context) throws IOException, InterruptedException {
-
-			String original = key.toString();
-			MessageDigest md;
-			try {
-				md = MessageDigest.getInstance("MD5");
-				md.update(original.getBytes());
-				byte[] digest = md.digest();
-				StringBuffer md5Text = new StringBuffer();
-				for (byte b : digest) {
-					md5Text.append(String.format("%02x", b & 0xff));
-				}
-				
-				context.write(key, new Text(md5Text.toString()));
-				
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
-			
-
-		}
 	}
 
 	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		HBaseConfiguration.addHbaseResources(conf);
+		
+		Job job = new Job(conf, "bulkload");
+		job.setJarByClass(BulkLoad.class);
+
+		job.setNumReduceTasks(2);
+
+		job.setMapperClass(Map.class);
+
+		job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+		job.setMapOutputValueClass(KeyValue.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(HFileOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
 	}
 
 }
